35eb6f28ebd767647e261567cc0494f5ceea471e
oen142 <oen142@naver.com>
2021-09-30 21:58:29 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/service/ServiceApplyTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceApplyTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceApplyTutorialApplication>(*args)
+}
